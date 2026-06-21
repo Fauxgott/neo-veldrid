0890dfdecac886ff21e42b96712dcc537af2921d
Fauxgott <fauxgott@protonmail.ch>
2026-06-20 20:37:24 -0400

    Add D3D11 pre-initialization versioning method

--- src/NeoVeldrid/D3D11/D3D11GraphicsDevice.cs
+++ src/NeoVeldrid/D3D11/D3D11GraphicsDevice.cs
@@ -267,6 +267,58 @@ namespace NeoVeldrid.D3D11
             return d3D11DeviceOptions;
         }
 
+        internal static GraphicsApiVersion GetApiVersion()
+        {
+            GraphicsApiVersion result = GraphicsApiVersion.Unknown;
+
+            using var d3d11 = Silk.NET.Direct3D11.D3D11.GetApi(null);
+            D3DFeatureLevel[] featureLevels = new[]
+            {
+                // Only checks for D3D11 capabilities.
+                D3DFeatureLevel.Level111,
+                D3DFeatureLevel.Level110,
+            };
+
+            ComPtr<ID3D11Device> device = default;
+            ComPtr<ID3D11DeviceContext> context = default;
+            D3DFeatureLevel maxLevel;
+
+            unsafe
+            {
+                fixed (D3DFeatureLevel* pFeatureLevels = featureLevels)
+                {
+                    int hr = d3d11.CreateDevice(
+                        default(ComPtr<IDXGIAdapter>),
+                        D3DDriverType.Hardware,
+                        IntPtr.Zero,
+                        (uint)CreateDeviceFlag.None,
+                        pFeatureLevels,
+                        (uint)featureLevels.Length,
+                        Silk.NET.Direct3D11.D3D11.SdkVersion,
+                        ref device,
+                        &maxLevel,
+                        ref context);
+
+                    if (hr >= 0)
+                    {
+                        // Example: Level111 relates to 11.1.
+                        string levelStr = maxLevel.ToString().Replace("Level", "");
+                        if (levelStr.Length == 3)
+                        {
+                            result = new GraphicsApiVersion(
+                                Convert.ToInt32(levelStr[..2]),
+                                Convert.ToInt32(levelStr[2..]),
+                                0, 0);
+                        }
+                    }
+                }
+            }
+
+            context.Dispose();
+            device.Dispose();
+            return result;
+        }
+
         private protected override void SubmitCommandsCore(CommandList cl, Fence fence)
         {
             D3D11CommandList d3d11CL = Util.AssertSubtype<CommandList, D3D11CommandList>(cl);
