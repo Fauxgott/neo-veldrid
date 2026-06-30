57a474df124cf3b6c04f9acc36b1f3aedc463d14
Fauxgott <fauxgott@protonmail.ch>
2026-06-30 12:40:24 -0400

    Fix API version type to report as known

--- src/NeoVeldrid/GraphicsApiVersion.cs
+++ src/NeoVeldrid/GraphicsApiVersion.cs
@@ -1,4 +1,4 @@
-﻿
+
 namespace NeoVeldrid
 {
     public readonly struct GraphicsApiVersion
@@ -10,7 +10,7 @@ namespace NeoVeldrid
         public int Subminor { get; }
         public int Patch { get; }
 
-        public bool IsKnown => Major != 0 && Minor != 0 && Subminor != 0 && Patch != 0;
+        public bool IsKnown => Major > 0 && Minor > 0 && Subminor >= 0 && Patch >= 0;
 
         public GraphicsApiVersion(int major, int minor, int subminor, int patch)
         {
